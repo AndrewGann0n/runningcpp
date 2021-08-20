0d65a75c72415adc48e6fc90667ac9f82f926542
Andrew <andrewpgannon1@gmail.com>
2021-08-20 19:17:18 -0400

    new work

--- main.cc
+++ main.cc
@@ -5,8 +5,17 @@
 using namespace std;
 
 
+
+
 int main()
 {
+    Races rw;
+
+    Races *interface1 = &rw;
+
+    interface1->kilorace();
+
+
     cout << "Welcome" << endl;
     cout << "This is a program used to calculate running progress" << endl;
     cout << "Enter the number of miles run to the nearest tenth" << endl;
