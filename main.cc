c59b47469d728703ee44f56dcd31484049a7a5b8
Andrew <andrewpgannon1@gmail.com>
2021-08-20 18:46:54 -0400

    new file

--- main.cc
+++ main.cc
@@ -8,11 +8,25 @@ double feet;
 double yards;
 
 void run(){
+    double miles;
+    if (miles > 3.26){
+        cout << "You ran at least a 5k";
+    }
+    if (miles > 7.52){
+        cout << "You ran at least a 10k";
+    }
     
 }
 
 int main()
 {
+    cout << "Welcome" << endl;
+    cout << "This is a program used to calculate running progress" << endl;
+    cout << "Enter the number of miles run to the nearest tenth" << endl;
+    cin >> miles;
+
+
+    run();
     
 
 }
